a2e04756869afb27e8ef0d58a0d4e4ef98f95290
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-09-15 11:14:53 +0200

    la till component repo

---
+++ components/src/components/Sample/Sample.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Sample  = () => {
+
+	return(
+		<>Sample</>
+	);
+}
+
+export default Sample;
---
+++ components/src/components/Sample/Sample.stories.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+//import Sample from './Sample';
+import {Sample} from '.';
+
+export default {
+	title: 'Components/Sample',
+	component: Sample, 
+}
+
+export const Default = {
+
+}
+
+export const BreakMyComponent = {
+
+}
