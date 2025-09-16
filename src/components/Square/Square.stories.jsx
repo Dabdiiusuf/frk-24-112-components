56207f6d040831988f098a93b64682690e4a85f6
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-09-16 14:30:49 +0200

    added 6 files to Square component

---
+++ src/components/Square/Square.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Square} from '.';
+
+export default {
+	title: 'Components/Square',
+	component: Square,
+}
+
+export const Default =  {
+
+}
+
+export const BreakMyComponent = {
+
+}
