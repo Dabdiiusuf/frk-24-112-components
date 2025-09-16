fc10338762097c1513a2d87e049eb9edd9ebf6df
Elias <elias@st-innovations.net>
2025-09-16 13:49:53 +0200

    added base Board component

---
+++ src/components/Board/Board.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Board} from '.';
+
+export default {
+	title: 'Components/Board',
+	component: Board,
+}
+
+export const Default = {
+
+}
+
+export const BreakMyComponent = {
+
+}
