dda2571ec200b36295ea133bcf2a691e0d9f37d6
Bassam <shahoudbassam@gmail.com>
2025-09-18 19:55:19 +0200

    Create atom for resetFunction

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,13 +1,23 @@
 import Button from './Button.molecule';
+import Reset from './Reset.atom';
+
 
 export default {
     title: 'Components/Button',
     component: Button,
 };
 
-export const Default = {
+// --- Button stories ---
+export const DefaultButton = {
     args: {
         label: 'Click Me',
         onClick: () => alert('Button clicked!'),
     },
 };
+
+// --- Reset stories ---
+export const ResetButton = {
+    render: () => <Reset onReset={() => alert('Game reset!')} />,
+};
+
+
