087fc73167effb5f8e8b78e4ff3767f415e11a94
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-09-19 10:13:27 +0200

    Changed Counter comp to Points comp

---
+++ src/components/Points/Points.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Points} from '.';
+
+export default {
+	title: 'Components/Points',
+	component: Points,
+}
+
+export const Default = {
+
+}
+
+export const BreakMyComponent = {
+
+}
