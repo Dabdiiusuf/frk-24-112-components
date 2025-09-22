5959c27a08d2812d7c3acb6d90a704042407d118
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-09-22 15:24:40 +0200

    Added new component Player1

---
+++ src/components/Player1/Player1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Player1 = () => {
+	return (
+		<>Player1</>
+	);
+};
+
+export default Player1;
---
+++ src/components/Player1/Player1.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Player1} from '.';
+
+export default {
+	title: 'Components/Player1',
+	component: Player1,
+}
+
+export const Default = {
+
+}
