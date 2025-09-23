a788d4e03b6a1aa92ab93654ea9571986189dab3
Elias <elias@st-innovations.net>
2025-09-23 15:11:35 +0200

    Modal Window image now show in storybook

--- src/components/ModalWindow/ModalWindow.molecule.jsx
+++ src/components/ModalWindow/ModalWindow.molecule.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./ModalWindow.module.css";
 
 const ModalWindow = () => {
-  return <>Modal window</>;
+  return(
+	<div className={styles.WoodBox}>
+		
+	</div>
+) 
+
+
 };
 
 export default ModalWindow;
