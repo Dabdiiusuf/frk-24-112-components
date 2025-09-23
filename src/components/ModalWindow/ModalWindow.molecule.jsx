f9443d29b17fd0f42c43e0d36dd15811af6f6b80
Elias <elias@st-innovations.net>
2025-09-23 10:37:53 +0200

    created modal window molecule 6 files

---
+++ src/components/ModalWindow/ModalWindow.molecule.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ModalWindow = () => {
+  return <>Modal window</>;
+};
+
+export default ModalWindow;
---
+++ src/components/ModalWindow/ModalWindow.stories.jsx
@@ -0,0 +1,9 @@
+import { React } from "react";
+import { ModalWindow } from ".";
+
+export default {
+  title: "Components/ModalWindow",
+  component: ModalWindow,
+};
+
+export const Default = {};
