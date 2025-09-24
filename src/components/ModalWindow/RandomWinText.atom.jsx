f6cc25350b950546543e00f34b02988cb6da2d4a
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-09-24 17:16:41 +0200

    Added Random win text in Modal Window

--- src/components/ModalWindow/RandomWinText.atom.jsx
+++ src/components/ModalWindow/RandomWinText.atom.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 
-const RandomText = () => {
-	const RandomTextArray = 
-		[
-			"Yo-ho-ho! The match be over, and Daniel claims the treasure o’ triumph! The rest be feedin’ the fishes!", 
-			"Shiver me timbers! The game’s end has come, and it be Daniel who hoists the black flag o’ victory!",
-			"Arrr, the board be conquered! Daniel rules these seas, leavin’ naught but wreckage in his wake!",
-			"Game over, ye scallywags! Daniel be the last pirate standin’, with the crown o’ glory upon his head!",
-			"Blimey! The cannons be silenced and the battle be won. Daniel sails away with victory’s bounty!",
-			"Arrr! The seas be still, the battle be done... Cap’n Daniel stands victorious upon the bones of his foes!",	 	
-		];
+export const RandomTextArray =
+                [       
+                        "Yo-ho-ho! The match be over, and Daniel claims the treasure o’ triumph! The rest be feedin’ the fishes!",
+                        "Shiver me timbers! The game’s end has come, and it be Daniel who hoists the black flag o’ victory!",
+                        "Arrr, the board be conquered! Daniel rules these seas, leavin’ naught but wreckage in his wake!",
+                        "Game over, ye scallywags! Daniel be the last pirate standin’, with the crown o’ glory upon his head!",
+                        "Blimey! The cannons be silenced and the battle be won. Daniel sails away with victory’s bounty!",
+                        "Arrr! The seas be still, the battle be done... Cap’n Daniel stands victorious upon the bones of his foes!",
+                ];
 
+const RandomText = () => {
 	return(
-		<div>
-			{RandomTextArray && (
-					RandomTextArray.map((text, index) => (
-							<div>{text}</div>
-						)
-					)
-				)
-			}
-		</div>
+		<div></div>
 	);
 };
 
