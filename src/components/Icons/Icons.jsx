4abbba0e2b22152be8a3ba87501091571ae21b00
Elias <elias@st-innovations.net>
2025-09-25 11:55:54 +0200

    added shadows to icons and exampleButtons for a more realistic look

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import X from './Images/X.png';
-import O from  './Images/O.png';
+import React from "react";
+import X from "./Images/X.png";
+import O from "./Images/O.png";
+import styles from "./icons.module.css";
 
 const Icons = () => {
-
-	return(
-		<div>
-			<img src={X} alt="X icon" />
-			<img src={O} alt="O icon" />
-		</div>
-	);
-}
+  return (
+    <div>
+      <img src={X} alt="X icon" className={styles.shadow} />
+      <img src={O} alt="O icon" className={styles.shadow} />
+    </div>
+  );
+};
 
 export default Icons;
