dcec176d542132a5ff05d0093b246f2c828a9a0c
Elias <elias@st-innovations.net>
2025-09-25 17:00:36 +0200

    implemented start/play again button in modal window

--- src/components/ModalWindow/ModalWindow.molecule.jsx
+++ src/components/ModalWindow/ModalWindow.molecule.jsx
@@ -1,10 +1,12 @@
-import {useState} from "react";
+import { useState } from "react";
 import styles from "./ModalWindow.module.css";
-import { RandomTextArray } from './RandomWinText.atom';
+import { RandomTextArray } from "./RandomWinText.atom";
+import StartPlayAgainBtn from "../Button/ExampleStartPlayAgain.atom";
 
 const ModalWindow = () => {
-  const textIndex = Math.floor(Math.random() * RandomTextArray.length);  
+  const textIndex = Math.floor(Math.random() * RandomTextArray.length);
   const [randomText, setRandomText] = useState(RandomTextArray[textIndex]);
+  const [started, setStarted] = useState(false);
 
   return (
     <div className={styles.woodBox}>
@@ -22,7 +24,13 @@ const ModalWindow = () => {
             input here!
           </div>
         </div>
-        <div className={styles.btn}>import button here!</div>
+        <div className={styles.btn}>
+          <StartPlayAgainBtn
+            started={started}
+            onStart={() => setStarted(true)}
+            onPlayAgain={() => setStarted(false)}
+          />
+        </div>
       </div>
     </div>
   );
