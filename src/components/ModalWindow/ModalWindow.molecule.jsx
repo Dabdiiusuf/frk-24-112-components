7a387c8ddf9c0e21714db68334ac76cc155981d4
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-09-29 19:56:34 +0200

    Added a ConfigProvider

--- src/components/ModalWindow/ModalWindow.molecule.jsx
+++ src/components/ModalWindow/ModalWindow.molecule.jsx
@@ -1,37 +1,9 @@
 import { useState, useContext } from "react";
 import styles from "./ModalWindow.module.css";
-import StartPlayAgainBtn from "../Button/ExampleStartPlayAgain.atom";
-import PlayerOneInput from "../Players/FirstPlayer.atom";
-import PlayerTwoInput from "../Players/SecondPlayer.atom";
 import { GomokuContext } from "../../providers/GomokuContext";
 
 const ModalWindow = () => {
-  const [started, setStarted] = useState(false);
-
-  return (
-    <div className={styles.woodBox}>
-      {/* <div className={styles.layout}>
-        <RandomWinText />
-        <div className={styles.header}> Import Game Name / Game Over here!</div>
-        <div className={styles.message}>{randomText}/rules here!</div>
-        <div className={styles.inputBox}>
-          <div className={styles.player1}>
-            <PlayerOneInput />
-          </div>
-          <div className={styles.player2}>
-            <PlayerTwoInput />
-          </div>
-        </div>
-        <div className={styles.btn}>
-          <StartPlayAgainBtn
-            started={started}
-            onStart={() => setStarted(true)}
-            onPlayAgain={() => setStarted(false)}
-          />
-        </div>
-      </div> */}
-    </div>
-  );
+  return <div className={styles.woodBox}></div>;
 };
 
 export default ModalWindow;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { GomokuContextProvider } from "./providers/GomokuContext.jsx";
+import { ConfigContextProvider } from "./providers/ConfigContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <GomokuContextProvider>
-    <App />
-  </GomokuContextProvider>
+  <ConfigContextProvider>
+    <GomokuContextProvider>
+      <App />
+    </GomokuContextProvider>
+  </ConfigContextProvider>
 );
