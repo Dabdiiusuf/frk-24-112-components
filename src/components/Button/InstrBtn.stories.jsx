6466b1404d0f1843a12e8ec76ec2245d33a8c323
steelhammers <tiamsaramolki11@gmail.com>
2025-09-29 13:29:59 +0200

    minor correction

--- src/components/Button/InstrBtn.stories.jsx
+++ src/components/Button/InstrBtn.stories.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import InstrBtn from './InstrBtn.atom';
 
 export default {
-	title: 'Components/Button/InstrBtn',
+	title: 'Components/Button/InstrBtn.atom',
 	component: InstrBtn,
 }
 
