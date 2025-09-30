37926e586b2e93953811119f961774c7e34fdeb1
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-09-30 09:52:23 +0200

    Removed Providers

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { GomokuContextProvider } from "./providers/GomokuContext.jsx";
-import { ConfigContextProvider } from "./providers/ConfigContext.jsx";
-import { ApiContextProvider } from "./providers/ApiProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <ConfigContextProvider>
-    <ApiContextProvider>
-      <GomokuContextProvider>
-        <App />
-      </GomokuContextProvider>
-    </ApiContextProvider>
-  </ConfigContextProvider>
+  <GomokuContextProvider>
+    <App />
+  </GomokuContextProvider>
 );
