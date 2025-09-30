4bdd779eceb72f8e9f9f2ccabb84233295498e19
Elias <elias@st-innovations.net>
2025-09-30 13:42:17 +0200

    added index for link to work for frontend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-export {GameBoard} from './components'
+export { default as Background } from "./components/Background/Background";
+export { default as GameBoard } from "./components/GameBoard/GameBoard.molecule";
+export { default as Board } from "./components/GameBoard/Board.atom";
+export { default as GCP } from "./components/GameControlPanel/GameControlPanel";
+export { default as Icons } from "./components/Icons/Icons";
+export { default as ModalWindow } from "./components/ModalWindow/ModalWindow.molecule";
