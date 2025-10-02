36d658eca0ad9f56ec20693f7f57157e5f1275b2
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-10-02 17:41:55 +0200

    Added GameOver in background

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -4,16 +4,17 @@ import GameBoard from "../GameBoard/GameBoard.molecule";
 import GameControlPanel from "../GameControlPanel/GameControlPanel";
 
 const Background = ({ children }) => {
-  const [gameArea = null, modalWindow = null, controlPanel = null] =
-    React.Children.toArray(children);
+  const [
+    gameArea = null,
+    modalWindow = null,
+    gameOver = null,
+    controlPanel = null,
+  ] = React.Children.toArray(children);
 
   return (
     <div className={styles.bgBox}>
       <div className={styles.header}>
-        <h1
-          className={styles.gameName}
-          data-text="Pirate Gomoku"
-        >
+        <h1 className={styles.gameName} data-text="Pirate Gomoku">
           Pirate Gomoku
         </h1>
       </div>
@@ -21,6 +22,7 @@ const Background = ({ children }) => {
       <div>{gameArea}</div>
       <div>{controlPanel}</div>
       <div>{modalWindow}</div>
+      <div>{gameOver}</div>
     </div>
   );
 };
