6a0104b54491117ea57bc34f8fdf6269469f5839
Elias <elias@st-innovations.net>
2025-10-02 15:00:34 +0200

    gcp now show names, but they are not saved

--- src/components/GameControlPanel/GameControlPanel.jsx
+++ src/components/GameControlPanel/GameControlPanel.jsx
@@ -4,10 +4,7 @@ import ResetBtn from "../Button/Reset.atom";
 import TimerButton from "../Button/ExampleTimerButton.atom";
 import Timer from "../Timer/Timer";
 
-const GameControlPanel = ({
-  FirstPlayer = "Daniel",
-  SecondPlayer = "Player 2",
-}) => {
+const GameControlPanel = ({ playerOne, playerTwo }) => {
   const [running, setRunning] = useState(false);
   const [startSignal, setStartSignal] = useState(0);
 
@@ -21,13 +18,13 @@ const GameControlPanel = ({
       <div></div>
       <div className={styles.stoneBoard}>
         <div className={styles.firstStone}>
-          <p>{FirstPlayer}</p>
+          <p>{playerOne}</p>
           <p>Points: 3</p>
           <p>Icon</p>
           <p>"Playing"</p>
         </div>
         <div className={styles.secondStone}>
-          <p>{SecondPlayer}</p>
+          <p>{playerTwo}</p>
           <p>Points: 1</p>
           <p>Icon</p>
           <p>Done</p>
@@ -35,7 +32,7 @@ const GameControlPanel = ({
       </div>
       <div className={styles.message}>
         <p className={styles.playerTurn}>
-          ARRR, IT BE <span className={styles.Player}>{FirstPlayer}</span> ONE'S
+          ARRR, IT BE <span className={styles.Player}>{playerOne}</span> ONE'S
           TURN! MAKE YER MOVE, MATEY, AFORE THE TIDE CHANGES!
         </p>
         <p className={styles.displayTimer}>
@@ -49,7 +46,10 @@ const GameControlPanel = ({
         </p>
       </div>
       <div className={styles.timerReset}>
-        <TimerButton running={running} onStart={handleStart} />
+        <TimerButton
+          running={running}
+          onStart={handleStart}
+        />
         <ResetBtn />
       </div>
     </div>
