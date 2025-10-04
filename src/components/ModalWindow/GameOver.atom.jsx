73200b5a92a4dbb2c67e791024e895861c48519e
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-10-04 21:01:38 +0200

    added ternary for icon switch on winner

--- src/components/ModalWindow/GameOver.atom.jsx
+++ src/components/ModalWindow/GameOver.atom.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import styles from "./ModalWindow.module.css";
 import { PlayAgainButton } from "../Button/ExampleStartPlayAgain.atom";
 import X from "../Icons/Images/X.png";
+import O from "../Icons/Images/O.png";
 
-const GameOver = ({ GameOverText, playAgain }) => {
+const GameOver = ({ GameOverText, playAgain, firstIcon }) => {
   return (
     <div className={styles.woodBox}>
       <div className={styles.layout}>
@@ -11,7 +12,11 @@ const GameOver = ({ GameOverText, playAgain }) => {
         <div className={styles.paper} aria-hidden />
         <div className={styles.randomMessage}>
           <div>{GameOverText}</div>
-          <img src={X} alt="X icon" className={styles.icon} />
+          {firstIcon ? (
+            <img src={X} alt="X icon" className={styles.icon} />
+          ) : (
+            <img src={O} alt="O icon" className={styles.icon} />
+          )}
         </div>
         <div className={styles.btn}>
           <PlayAgainButton playAgain={playAgain} />
